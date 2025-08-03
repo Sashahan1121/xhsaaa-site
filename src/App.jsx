72e6815cc80aeaf5ed5b97e2129e177313f8336a
Sashahan1121 <haoh4936@gmail.com>
2025-08-04 00:24:50 +0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>📚 小红书读书推荐生成器</h1>
+      <p>你可以输入书名、情绪、关键词，我来帮你生成适合小红书的推荐文案！</p>
+    </div>
+  )
+}
+
+export default App
